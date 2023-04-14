1a7d9adce209db11a9d6e2da16caff858e94c75d
Artem <artemchernogor.on@gmail.com>
2023-04-14 18:00:56 +0200

    Сделал в один запрос и апдейт.

--- dbo.TR_Basket_insert.sql
+++ dbo.TR_Basket_insert.sql
@@ -2,22 +2,18 @@ CREATE TRIGGER TR_Basket_insert
 ON dbo.Basket
 AFTER INSERT
 AS
-	DECLARE @sku INT = 0;
-	
-	SELECT @sku = A.ID_SKU
-	FROM
-		(SELECT ID_SKU, COUNT(*) AS CNT
-		FROM inserted 
-		GROUP BY ID_SKU) AS A 
-	WHERE A.CNT >= 2
+  DECLARE @sku INT = 0;  
+  
+  SELECT @sku = ID_SKU
+  FROM inserted 
+  GROUP BY ID_SKU
+  HAVING COUNT(ID_SKU) >= 2
+    
+   
 
-	IF @sku = 0
-	BEGIN
-		RETURN;
-	END;
-
-	UPDATE dbo.Basket 
-	SET DiscountValue = Value * 0.05
-	WHERE ID_SKU = @sku;
- 
-GO
+ UPDATE dbo.Basket 
+  SET DiscountValue = CASE
+      WHEN  @sku >= 2 THEN VALUE * 0.05
+	  ELSE 0
+    END
+ WHERE ID_SKU = @sku 
