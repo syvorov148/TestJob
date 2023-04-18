db610f9acf13efac03a1034e2192ced8b78b69a9
Artem <artemchernogor.on@gmail.com>
2023-04-18 22:31:42 +0200

    Исправил триггер.

--- dbo.TR_Basket_insert.sql
+++ dbo.TR_Basket_insert.sql
@@ -2,18 +2,19 @@ CREATE TRIGGER TR_Basket_insert
 ON dbo.Basket
 AFTER INSERT
 AS
-  DECLARE @sku INT = 0;  
+  DECLARE @sku INT = 0;
+  DECLARE @sku_count INT = 0;
   
-  SELECT @sku = ID_SKU
+  SELECT @sku = ID_SKU, @sku_count = COUNT(ID_SKU)
   FROM inserted 
   GROUP BY ID_SKU
-  HAVING COUNT(ID_SKU) >= 2
-    
-   
+  ORDER BY COUNT(ID_SKU)
 
- UPDATE dbo.Basket 
+  UPDATE dbo.Basket 
   SET DiscountValue = CASE
-      WHEN  @sku >= 2 THEN VALUE * 0.05
-	  ELSE 0
+      WHEN  @sku_count >= 2 THEN VALUE * 0.05
+	  ELSE VALUE * 0
     END
- WHERE ID_SKU = @sku 
+ WHERE ID_SKU = @sku
+	
+		
