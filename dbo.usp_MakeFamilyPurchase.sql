8bf4a08474769fc9f5a2bf258010688ba3e735a9
Artem <artemchernogor.on@gmail.com>
2023-04-19 12:03:50 +0200

    Исправил

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,11 +1,16 @@
-CREATE PROCEDURE dbo.usp_MakeFamilyPurchase
-	@FamilySurName VARCHAR(255)
-AS
-	DECLARE @familyId INT =0;
-	SELECT @familyId = ID FROM dbo.Family WHERE SurName = @FamilySurName;
-	IF @familyId = 0
-		PRINT 'Такой семьи нет'
-	UPDATE dbo.Family
-	SET BudgetValue = (SELECT SUM(Value) FROM dbo.Basket WHERE ID_Family = @familyId )
+create procedure dbo.usp_MakeFamilyPurchase 
+	@FamilySurName varchar(255)
+as
+	declare @familyId int =0;
+	select @familyId = ID 
+	from dbo.Family 
+	where SurName = @FamilySurName;
+	if @familyId = 0
+		print 'Такой семьи нет'
+	update dbo.Family
+	set BudgetValue = 
+	(select sum(Value) 
+	from dbo.Basket 
+	where ID_Family = @familyId)
 GO
 
